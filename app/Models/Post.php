7ad989ca997a8a12607ba79af110d52add228e06
akandeseun <akandeseun44@gmail.com>
2023-10-31 11:55:57 +0100

    post->tag relationship

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -4,10 +4,16 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Post extends Model
 {
     use HasFactory;
 
     protected $fillable = ['title', 'excerpt', 'body'];
+
+    public function tags(): HasMany
+    {
+        return $this->hasMany(Tag::class);
+    }
 }
--- database/migrations/2023_10_30_133412_create_posts_table.php
+++ database/migrations/2023_10_30_133412_create_posts_table.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Models\Category;
+use App\Models\Tag;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -16,6 +18,8 @@ return new class extends Migration
             $table->string('title');
             $table->string('excerpt');
             $table->text('body');
+            $table->foreignIdFor(Tag::class);
+            $table->foreignIdFor(Category::class);
             $table->timestamps();
         });
     }
