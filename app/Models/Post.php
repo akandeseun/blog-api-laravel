31b5de91d7a48be271094ee8cc66dfd7e3e9c814
akandeseun <akandeseun44@gmail.com>
2023-10-31 12:15:32 +0100

    post -> category relationships

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Relations\HasOne;
 
 class Post extends Model
 {
@@ -15,5 +16,10 @@ class Post extends Model
     public function tags(): HasMany
     {
         return $this->hasMany(Tag::class);
+    }
+
+    public function category(): HasOne
+    {
+        return $this->hasOne(Category::class);
     }
 }
